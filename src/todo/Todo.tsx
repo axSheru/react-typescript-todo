a9bd9b101376b099841fb54f9d8d77088b44ec20
Alex Paredes <alexparedesr95@gmail.com>
2022-11-08 13:11:00 -0600

    REFACTOR: Implemented Title component.

--- src/todo/Todo.tsx
+++ src/todo/Todo.tsx
@@ -1,10 +1,11 @@
 import { TodoList } from './components/TodoList';
 import { TodoProvider } from './context/TodoProvider';
+import { Title } from './components/Title';
 
 export const Todo = () => {
     return (
         <TodoProvider>
-            <h1>Todo:</h1>
+            <Title />
             <TodoList />
         </TodoProvider>
     );
